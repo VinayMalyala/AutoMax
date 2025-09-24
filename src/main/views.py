2ee655693e6032f5e916de5755cc8f7bf08443b6
Vinay Malyala <srivinay1432@gmail.com>
2025-09-24 12:07:28 +0530

    render main.html

--- src/main/views.py
+++ src/main/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def main_view(request):
-    return HttpResponse("This is the Landing Page")
+    return render(request, 'views/main.html')
