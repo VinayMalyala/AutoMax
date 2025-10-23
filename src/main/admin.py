686852421a3910880ce2e917511ce5ce89ef9c61
Vinay Malyala <srivinay1432@gmail.com>
2025-10-23 20:20:04 +0530

    add listing model

--- src/main/admin.py
+++ src/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Listing
 
 # Register your models here.
+class ListingAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Listing, ListingAdmin)
