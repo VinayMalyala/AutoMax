dfbb65912d8b934b22dd14f48bf965c6e2d79272
Vinay Malyala <srivinay1432@gmail.com>
2025-11-26 12:32:53 +0530

    configuring email service django

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -148,6 +148,14 @@ MEDIA_URL = '/media/'
 # Django Crispy Form Settings
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
