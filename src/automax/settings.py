a83669635f0fa9d8ed41bbc9dab648f7a083ad41
Vinay Malyala <srivinay1432@gmail.com>
2025-11-28 12:30:42 +0530

    update allowed hosts in setting.py

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(env('DEBUG'))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
